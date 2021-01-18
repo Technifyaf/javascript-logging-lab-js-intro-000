4c0160362f2c2678b57c930c3e40b8862cc74ded
Thato Rammoko <technifyaf@gmail.com>
2021-01-18 05:24:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.error("HALP!")
+console.error(" Uh oh see what you did now, you broke it!")
+console.warn("You are on slippery slope my friend");
+console.log("I know what you did last summer");
